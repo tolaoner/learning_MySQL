5cb37c4b042ff435a09a994f5da4647d7c045c20
tolaoner <59030417+tolaoner@users.noreply.github.com>
2020-10-09 16:08:35 +0200

    avg, sum, cound , round
    some new commands

---
+++ avg_count_sum.sql
@@ -0,0 +1,5 @@
+#avg(column)---takes the average of the values of given column in parenthesis
+#sum(column)---gives the sum of the values of given column in parenthesis
+#count--gives the no. appearance of a value in a column
+#round(value,decimal count)
+# you can use case when in where statement
